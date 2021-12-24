efbad6ec732ab03053f10cd6a759a8ba6ea3229c
hongchan88 <33241580+hongchan88@users.noreply.github.com>
2021-12-25 09:40:21 +1100

    update from sandbox

--- pages/films/[id].js
+++ pages/films/[id].js
@@ -1,25 +1,165 @@
-import React from "react";
-import { useRouter } from "next/router";
-import Characters from "../../components/characters";
+import React, { useEffect, useState } from "react";
+import styled from "./films.module.scss";
+import Character from "../../components/character";
 import Link from "next/link";
 import Header from "../../components/header";
+import { AnimatePresence, motion } from "framer-motion";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
+const boxVariants = {
+  normal: {
+    scale: 1
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.3,
+      duaration: 0.3,
+      type: "tween"
+    }
+  }
+};
 const FilmsbyId = ({ dataFilmsById, characterData }) => {
-  console.log(characterData);
-  const { title, directed, producer, characters } =
-    dataFilmsById.result.properties;
-  const router = useRouter();
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const [direction, setDirection] = useState(true);
+
+  const [innerWidth, setInnerWidth] = useState();
+
+  const {
+    title,
+    directed,
+    producer,
+    characters: charactersApi
+  } = dataFilmsById.result.properties;
+  console.log(charactersApi?.length, "api");
+  const offset = 6;
+  const divStyle = {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: "60px",
+    backgroundImage:
+      'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),url("https://res.cloudinary.com/dwbsxpk82/image/upload/v1640179209/portfolio/kki2so836ji1ptws2epr.jpg")',
+    backgroundSize: "cover"
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setInnerWidth(window.innerWidth);
+    });
+  }, []);
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+  }, []);
+  console.log(innerWidth);
+  const incraseIndex = () => {
+    if (!innerWidth) {
+      alert("getting user's window size");
+      return;
+    }
+    setDirection(true);
+    if (characterData) {
+      console.log(leaving);
+      if (leaving) return;
+      const totalMovies = characterData.length;
+      const maxIndex = Math.floor(totalMovies / offset);
+      toggleLeaving();
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const decreaseIndex = () => {
+    setDirection(false);
+    if (!innerWidth) {
+      alert("getting user's window size");
+      return;
+    }
+    if (characterData) {
+      console.log(leaving);
+      if (leaving) return;
+      const totalMovies = characterData.length;
+      console.log(totalMovies);
+      const maxIndex = Math.floor(totalMovies / offset);
+      toggleLeaving();
+
+      setIndex((prev) => (prev === 0 ? maxIndex : prev - 1));
+    }
+  };
 
   return (
     <>
       <Header />
-      <div>
-        <h1>{title}</h1>
+      <div style={divStyle}></div>
+      <div className={styled.slider}>
+        <div className={styled.gridcontainer}>
+          <div className={styled.back} onClick={decreaseIndex}>
+            <motion.p whileHover={{ scale: 1.3 }} className={styled.insidegrid}>
+              <FaArrowLeft size={50} />
+            </motion.p>
+          </div>
+          <AnimatePresence
+            initial={false}
+            custom={direction}
+            onExitComplete={toggleLeaving}
+          >
+            <motion.div
+              className={styled.row}
+              custom={direction}
+              initial={(direction) => {
+                return {
+                  x: direction ? innerWidth + 5 : -innerWidth - 5,
+                  opacity: 0.2
+                };
+              }}
+              animate={{
+                x: 0,
+                opacity: 1
+              }}
+              exit={(direction) => {
+                return {
+                  x: direction ? -innerWidth - 5 : innerWidth + 5,
+                  opacity: 0.2
+                };
+              }}
+              transition={{
+                type: "tween",
+                duration: 1
+              }}
+              key={index}
+            >
+              {characterData
+                ?.slice(offset * index, offset * index + offset)
+                .map((character) => (
+                  <motion.div
+                    variants={boxVariants}
+                    initial="normal"
+                    whileHover="hover"
+                    className={styled.box}
+                    transition={{ type: "tween" }}
+                    key={character.uid}
+                    // onClick={() => router.push(`/films/${movie.uid}`)}
+                  >
+                    {
+                      <Character
+                        character={character}
+                        charactersApi={charactersApi}
+                      />
+                    }
+                  </motion.div>
+                ))}
+            </motion.div>
+          </AnimatePresence>
+          <div className={styled.next} onClick={incraseIndex}>
+            <motion.p whileHover={{ scale: 1.3 }} className={styled.insidegrid}>
+              <FaArrowRight size={50} />
+            </motion.p>
+          </div>
+        </div>
       </div>
 
-      {characterData.map((character) => {
-        return <Characters character={character} />;
-      })}
       <Link href="/">Back Home</Link>
     </>
   );
@@ -33,7 +173,7 @@ export async function getStaticPaths() {
 
   // Get the paths we want to pre-render based on posts
   const paths = films.result.map((film) => ({
-    params: { id: film.uid },
+    params: { id: film.uid }
   }));
 
   // We'll pre-render only these paths at build time.
@@ -58,7 +198,7 @@ export async function getStaticProps({ params }) {
 
   // Promise.all for using map function
   const characterData = await Promise.all(
-    characters.slice(0, 3).map(async (characterUrl) => {
+    characters.slice(0, 8).map(async (characterUrl) => {
       const data = await getCharData(characterUrl);
 
       return data;
@@ -73,11 +213,11 @@ export async function getStaticProps({ params }) {
 
   if (!dataFilmsById || !characterData) {
     return {
-      notFound: true,
+      notFound: true
     };
   }
 
   return {
-    props: { dataFilmsById, characterData }, // will be passed to the page component as props
+    props: { dataFilmsById, characterData } // will be passed to the page component as props
   };
 }
