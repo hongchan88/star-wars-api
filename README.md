162b887d09cc3d1027d7fffe5987dc093b47c188
unknown <hongchan88@hotmail.com>
2021-12-27 00:32:25 +1100

    character page with tooltop box, overlay to push back

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 ## star wars api
+
+### pages and functions
+
+- [x] Home page
+
+- [x] Header navigation
+- [] fixed header + scroll to change background color
+- [x] search bar on header
+- [x] added motion to show active navigation with red dot
+
+- [x] Movies page
+- [x] showing list of the movies
+- [x] click to favourite movie/unfavourite
+- [x] favourite movie move to top of the list
+- [x] slider , hover motion using framer motion
+
+- [x] Each Movie page has list of characters
+- [x] hovering each character will show more info link
+- [x] more info will tooltip box with detail of the that character including height, ...
+- [x] clicking other than the detail tooltip box will return to list of the characters page
+- [x] hovering, slider on character page of the movie
+
+- [] Loading ...
+- [] search function
--- pages/films/[id].js
+++ pages/films/[id].js
@@ -3,12 +3,13 @@ import styled from "./films.module.scss";
 import Character from "../../components/character";
 import Link from "next/link";
 import Header from "../../components/header";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useViewportScroll } from "framer-motion";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 const boxVariants = {
   normal: {
-    scale: 1
+    scale: 1,
   },
   hover: {
     scale: 1.3,
@@ -16,9 +17,20 @@ const boxVariants = {
     transition: {
       delay: 0.3,
       duaration: 0.3,
-      type: "tween"
-    }
-  }
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
+      type: "tween",
+    },
+  },
 };
 const FilmsbyId = ({ dataFilmsById, characterData }) => {
   const [index, setIndex] = useState(0);
@@ -26,12 +38,15 @@ const FilmsbyId = ({ dataFilmsById, characterData }) => {
   const [direction, setDirection] = useState(true);
 
   const [innerWidth, setInnerWidth] = useState();
+  const [characterQuery, setCharacterQuery] = useState(null);
 
+  const router = useRouter();
+  const { scrollY } = useViewportScroll();
   const {
     title,
     directed,
     producer,
-    characters: charactersApi
+    characters: charactersApi,
   } = dataFilmsById.result.properties;
   console.log(charactersApi?.length, "api");
   const offset = 6;
@@ -40,10 +55,10 @@ const FilmsbyId = ({ dataFilmsById, characterData }) => {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    padding: "60px",
+
     backgroundImage:
       'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),url("https://res.cloudinary.com/dwbsxpk82/image/upload/v1640179209/portfolio/kki2so836ji1ptws2epr.jpg")',
-    backgroundSize: "cover"
+    backgroundSize: "cover",
   };
 
   useEffect(() => {
@@ -88,79 +103,148 @@ const FilmsbyId = ({ dataFilmsById, characterData }) => {
       setIndex((prev) => (prev === 0 ? maxIndex : prev - 1));
     }
   };
+  const onMoreInfoClicked = (character) => {
+    const characterId = character.result.uid;
+    router.push(
+      {
+        pathname: `/films/${dataFilmsById.result.uid}`,
+        query: { characterId: characterId },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+
+  useEffect(() => {
+    setCharacterQuery(router.query.characterId);
+  }, [router.query.characterId]);
+
+  const clickOverlay = () => {
+    router.push(
+      {
+        pathname: `/films/${dataFilmsById.result.uid}`,
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
 
   return (
     <>
       <Header />
-      <div style={divStyle}></div>
-      <div className={styled.slider}>
-        <div className={styled.gridcontainer}>
-          <div className={styled.back} onClick={decreaseIndex}>
-            <motion.p whileHover={{ scale: 1.3 }} className={styled.insidegrid}>
-              <FaArrowLeft size={50} />
-            </motion.p>
-          </div>
-          <AnimatePresence
-            initial={false}
-            custom={direction}
-            onExitComplete={toggleLeaving}
-          >
-            <motion.div
-              className={styled.row}
-              custom={direction}
-              initial={(direction) => {
-                return {
-                  x: direction ? innerWidth + 5 : -innerWidth - 5,
-                  opacity: 0.2
-                };
-              }}
-              animate={{
-                x: 0,
-                opacity: 1
-              }}
-              exit={(direction) => {
-                return {
-                  x: direction ? -innerWidth - 5 : innerWidth + 5,
-                  opacity: 0.2
-                };
-              }}
-              transition={{
-                type: "tween",
-                duration: 1
-              }}
-              key={index}
-            >
-              {characterData
-                ?.slice(offset * index, offset * index + offset)
-                .map((character) => (
+      <div style={divStyle}>
+        <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+          <AnimatePresence>
+            <>
+              {characterQuery ? (
+                <>
                   <motion.div
-                    variants={boxVariants}
-                    initial="normal"
-                    whileHover="hover"
-                    className={styled.box}
-                    transition={{ type: "tween" }}
-                    key={character.uid}
-                    // onClick={() => router.push(`/films/${movie.uid}`)}
-                  >
-                    {
-                      <Character
-                        character={character}
-                        charactersApi={charactersApi}
-                      />
-                    }
-                  </motion.div>
-                ))}
-            </motion.div>
+                    onClick={clickOverlay}
+                    className={styled.overlay}
+                  />
+                  <motion.div
+                    layoutId={router.query.characterId + ""}
+                    style={{
+                      position: "absolute",
+
+                      width: "40vw",
+                      height: "80vh",
+                      background: "red",
+                      top: scrollY.get() + 100,
+                      left: "0",
+                      right: "0",
+                      margin: "0 auto",
+                      zIndex: "1",
+                    }}
+                  ></motion.div>
+                </>
+              ) : null}
+            </>
           </AnimatePresence>
-          <div className={styled.next} onClick={incraseIndex}>
-            <motion.p whileHover={{ scale: 1.3 }} className={styled.insidegrid}>
-              <FaArrowRight size={50} />
-            </motion.p>
+        </div>
+        <div className={styled.slider}>
+          <div className={styled.gridcontainer}>
+            <div className={styled.back} onClick={decreaseIndex}>
+              <motion.p
+                whileHover={{ scale: 1.3 }}
+                className={styled.insidegrid}
+              >
+                <FaArrowLeft size={50} />
+              </motion.p>
+            </div>
+            <AnimatePresence
+              initial={false}
+              custom={direction}
+              onExitComplete={toggleLeaving}
+            >
+              <motion.div
+                className={styled.row}
+                custom={direction}
+                initial={(direction) => {
+                  return {
+                    x: direction ? innerWidth + 5 : -innerWidth - 5,
+                    opacity: 0.2,
+                  };
+                }}
+                animate={{
+                  x: 0,
+                  opacity: 1,
+                }}
+                exit={(direction) => {
+                  return {
+                    x: direction ? -innerWidth - 5 : innerWidth + 5,
+                    opacity: 0.2,
+                  };
+                }}
+                transition={{
+                  type: "tween",
+                  duration: 1,
+                }}
+                key={index}
+              >
+                {characterData
+                  ?.slice(offset * index, offset * index + offset)
+                  .map((character) => (
+                    <motion.div
+                      layoutId={character.result.uid + ""}
+                      variants={boxVariants}
+                      initial="normal"
+                      whileHover="hover"
+                      className={styled.box}
+                      transition={{ type: "tween" }}
+                      key={character.result.uid}
+                      // onClick={() => router.push(`/films/${movie.uid}`)}
+                    >
+                      {
+                        <Character
+                          character={character}
+                          charactersApi={charactersApi}
+                        />
+                      }
+                      <motion.div
+                        variants={infoVariants}
+                        className={styled.infobox}
+                      >
+                        <h4 onClick={() => onMoreInfoClicked(character)}>
+                          more info
+                        </h4>
+                      </motion.div>
+                    </motion.div>
+                  ))}
+              </motion.div>
+            </AnimatePresence>
+            <div className={styled.next} onClick={incraseIndex}>
+              <motion.p
+                whileHover={{ scale: 1.3 }}
+                className={styled.insidegrid}
+              >
+                <FaArrowRight size={50} />
+              </motion.p>
+            </div>
           </div>
         </div>
+        <Link href="/">Back Home</Link>
       </div>
-
-      <Link href="/">Back Home</Link>
     </>
   );
 };
@@ -173,7 +257,7 @@ export async function getStaticPaths() {
 
   // Get the paths we want to pre-render based on posts
   const paths = films.result.map((film) => ({
-    params: { id: film.uid }
+    params: { id: film.uid },
   }));
 
   // We'll pre-render only these paths at build time.
@@ -213,11 +297,11 @@ export async function getStaticProps({ params }) {
 
   if (!dataFilmsById || !characterData) {
     return {
-      notFound: true
+      notFound: true,
     };
   }
 
   return {
-    props: { dataFilmsById, characterData } // will be passed to the page component as props
+    props: { dataFilmsById, characterData }, // will be passed to the page component as props
   };
 }
